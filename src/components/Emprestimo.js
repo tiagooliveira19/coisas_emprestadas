335b807ef3c1e7e290ff9adfce58cfcd581d6e64
Tiago Oliveira <tiago@pluginweb.com.br>
2019-03-13 11:44:44 -0300

    Ajustes Cadastro Emprestimo

--- src/components/Emprestimo.js
+++ src/components/Emprestimo.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 import Styles from '../style/Estilo';
 
 export default class Emprestimo extends Component {
 
-    /* https://hackernoon.com/how-to-take-advantage-of-local-storage-in-your-react-projects-a895f2b2d3f2 */
-
     constructor(props) {
         super(props);
-        this.state = { newItem: '', list: [] };
+        //this.state = { newItem: '', list: [] };
+        this.state = { item: '', dataDev: '', list: [] };
     }
 
     updateInput(key, value) {
@@ -16,7 +15,7 @@ export default class Emprestimo extends Component {
     }
 
     addItem() {
-        const newItem = {
+        /* const newItem = {
             id: 1 + Math.random(),
             value: this.state.newItem.slice()
         };
@@ -25,22 +24,40 @@ export default class Emprestimo extends Component {
 
         list.push(newItem);
 
-        this.setState({ list, newItem: '' });
+        this.setState({ list, newItem: '' }); */
+
+        const obj = {
+            id: 1 + Math.random(),
+            item: this.state.item.slice(),
+            dataDev: this.state.dataDev.slice()
+        };
+
+        const list = [...this.state.list];
+
+        list.push(obj);
+
+        this.setState({ list, item: '', dataDev: '' });
     }
 
     deleteItem(id) {
         const list = [...this.state.list];
         const updateList = list.filter(item => item.id !== id);
+        
         this.setState({ list: updateList });
     }
 
     render() {
         return (
             <View style={Styles.viewPrincipal} >
-                <TextInput 
+                <TextInput
                    placeholder="Item Emprestado" 
-                   value={this.state.newItem}
-                   onChange={e => this.updateInput('newItem', e.target.value)} 
+                   value={this.state.item}
+                   onChangeText={item => this.updateInput('item', item)}
+                />
+                <TextInput
+                   placeholder="Data Devolução" 
+                   value={this.state.dataDev}
+                   onChangeText={data => this.updateInput('dataDev', data)}
                 />
                 <View style={Styles.btnCadastrar} >
                     <Button 
@@ -50,11 +67,16 @@ export default class Emprestimo extends Component {
                     />
                 </View>
                 <View>
-                   {this.state.list.map(item => {
-                      return (
-                          <View>{item.value}</View>
-                      );
-                   })}            
+                   {this.state.list.map(obj => (
+                       <View>
+                           <Text>
+                                {obj.item}
+                            </Text>
+                            <Text>
+                                {obj.dataDev}
+                            </Text>
+                        </View>
+                   ))}            
                 </View>
             </View>
         );
